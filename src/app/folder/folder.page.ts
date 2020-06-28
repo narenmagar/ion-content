021e3c63524c358fdade6cb58eaa36548d4bed1b
iqnowworldpay <narendra.thapamagar@worldpay.com>
2020-06-28 15:08:40 -0400

    NOJIRA | Naren | Initial Commit with mulitple ion-content inside ion-content with dynamic height of top ion-content

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import MockData from '../util/MockData';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +9,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  private contentDataSize = 7; // change it based on need.
+  private itemHeightConstant = 5.5; // change it based on need.
+  private items = MockData.carLessThan7();
+  // private items = MockData.carMoreThan7(); use this to check cars more than 7
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private el: ElementRef) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  ionViewWillEnter() {
+    // use directives for best practice.
+    const topcontent = [].slice.call((this.el.nativeElement as HTMLElement).getElementsByClassName('topcontent'));
+    // set the height dynamically based on the need.
+    // check the number or rows that will be soon in the screen
+    // if the number of items are less than it than set the height based on the number of items.
+    const len = this.items.cars.length;
+    if (len < this.contentDataSize) {
+      const height = 'height:' + len * this.itemHeightConstant + 'vh';
+      topcontent[0].setAttribute('style', height);
+    } else {
+      topcontent[0].setAttribute('style', 'height:50vh');
+    }
+  }
 }
---
+++ src/app/util/MockData.ts
@@ -0,0 +1,301 @@
+
+export default class MockData {
+    static carLessThan7() {
+        return {
+            name: 'John',
+            age: 30,
+            cars: [
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                }
+            ],
+            bikes: [
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                }, {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                }
+            ]
+        };
+
+    }
+    static carMoreThan7() {
+        return {
+            name: 'John',
+            age: 30,
+            cars: [
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                }
+            ],
+            bikes: [
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                },
+                {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                }, {
+                    name: 'Ford',
+                    models: [
+                        'Fiesta',
+                        'Focus',
+                        'Mustang'
+                    ]
+                },
+                {
+                    name: 'BMW',
+                    models: [
+                        '320',
+                        'X3',
+                        'X5'
+                    ]
+                },
+                {
+                    name: 'Fiat',
+                    models: [
+                        '500',
+                        'Panda'
+                    ]
+                }
+            ]
+        };
+    }
+}
+
+
